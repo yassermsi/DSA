bbe07b41c2d1d187568da461f2789813ae6ca5da
yaalmasri <se.yasseralmasri@gmail.com>
2025-08-09 15:42:31 +0300

    insertion sort

---
+++ Arrays/InsertionSort.java
@@ -0,0 +1,16 @@
+public class InsertionSort {
+       public static void main(String[] args) {
+              int[] arr = { 7, 12, 9, 11, 3 };
+              for (int i = 1; i < arr.length; i++) {
+                     int key = arr[i], j = i - 1;
+                     while (j >= 0 && arr[j] > key) {
+                            arr[j + 1] = arr[j];
+                            j--;
+                     }
+                     arr[j + 1] = key;
+              }
+              System.out.print("Sorted array: ");
+              for (int i : arr)
+                     System.out.print(i + " ");
+       }
+}
