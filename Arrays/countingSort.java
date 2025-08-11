d20f61905356ea004890285f6d1f9f238aebd776
yaalmasri <se.yasseralmasri@gmail.com>
2025-08-12 01:17:54 +0300

    change name

--- Arrays/countingSort.java
+++ Arrays/countingSort.java
@@ -1,4 +1,4 @@
-public class countingSort {
+public class CountingSort {
        public static void main(String[] args) {
               int[] arr = { 4, 2, 2, 6, 3, 3, 1, 6, 5, 2, 3 };
               int max = arr[0];
