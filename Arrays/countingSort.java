5fa00234c7bbc69d1a9cc85fe83f419f64022769
yaalmasri <se.yasseralmasri@gmail.com>
2025-08-12 01:08:59 +0300

    counting sort

---
+++ Arrays/countingSort.java
@@ -0,0 +1,26 @@
+public class countingSort {
+       public static void main(String[] args) {
+              int[] arr = { 4, 2, 2, 6, 3, 3, 1, 6, 5, 2, 3 };
+              int max = arr[0];
+              for (int n : arr) {
+                     if (n > max) {
+                            max = n;
+                     }
+              }
+              int[] count = new int[max + 1];
+              for (int n : arr) {
+                     count[n]++;
+              }
+              int idx = 0;
+              for (int i = 0; i < count.length; i++) {
+                     while (count[i] > 0) {
+                            arr[idx++] = i;
+                            count[i]--;
+                     }
+              }
+              System.out.print("Sorted array: ");
+              for (int i : arr) {
+                     System.out.print(i + " ");
+              }
+       }
+}
