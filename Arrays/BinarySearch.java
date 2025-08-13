f7fe36babc23e1b4825a44e8b51e456ab2fdb7da
yaalmasri <se.yasseralmasri@gmail.com>
2025-08-14 00:56:33 +0300

    recursive algo

--- Arrays/BinarySearch.java
+++ Arrays/BinarySearch.java
@@ -1,23 +1,21 @@
 public class BinarySearch {
-       public static int binarySearch(int[] arr, int target) {
-              int start = 0, end = arr.length - 1;
-              while (start < end) {
-                     int mid = (start + end) / 2;
-                     if (arr[mid] == target) {
-                            return mid;
-                     } else if (arr[mid] > target) {
-                            end = mid - 1;
-                     } else {
-                            start = mid + 1;
-                     }
+       public static int binarySearch(int[] arr, int start, int end, int target) {
+              if (start > end)
+                     return -1;
+              int mid = (start + end) / 2;
+              if (arr[mid] > target) {
+                     return binarySearch(arr, start, mid - 1, target);
+              } else if (arr[mid] < target) {
+                     return binarySearch(arr, mid + 1, end, target);
+              } else {
+                     return mid;
               }
-              return -1;
        }
 
        public static void main(String[] args) {
               int[] arr = { 1, 3, 5, 7, 9, 11, 13, 15, 17, 19 };
               int target = 15;
-              int result = binarySearch(arr, target);
+              int result = binarySearch(arr, 0, arr.length, target);
               String res = (result != 1)
                             ? "Value " + Integer.toString(target) + " found at index " + Integer.toString(result)
                             : "Value " + String.valueOf(target) + " not found";
